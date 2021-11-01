9e7653bbc130d7de100587d4d7f2e38c090f3a7f
Aleksey Dibikov <dibikovaleksey@gmail.com>
2021-11-01 16:51:34 +0200

    Updates

---
+++ static/js/main.08e81a7d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-05-movies"]=this["webpackJsonpgoit-react-hw-05-movies"]||[]).push([[0],{11:function(e,t,n){"use strict";n.r(t);var c=n(1),i=n.n(c),o=n(3),r=n.n(o),s=(n(8),n(9),n(0));var a=function(){return Object(s.jsx)("div",{className:"App",children:"hello"})};r.a.render(Object(s.jsx)(i.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))},8:function(e,t,n){},9:function(e,t,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.08e81a7d.chunk.js.map
